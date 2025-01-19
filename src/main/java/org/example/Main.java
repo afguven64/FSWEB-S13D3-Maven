2ab03127038317368124c25d97658f75e58ddde2
afguven64 <ahmetfatihguven@gmail.com>
2025-01-20 00:16:22 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Person person = new Person("Ahmet", "Güven", 32);
+        Person person1 = new Person("Furkan", "Güven", 7,
+                "Kütahya", "Black", true);
+        System.out.println(person.getFirstName());
+        System.out.println(person.isTeen());
+
+
+        Wall wall = new Wall(7,8);
+        wall.setHeight(-10);
+        System.out.println(wall.getHeight());
     }
+
 }
